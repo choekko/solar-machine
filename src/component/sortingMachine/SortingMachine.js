84cd9462de5810425c3e7437724a58475bcc9cbf
yeoncho <yeoncho@student.42seoul.kr>
2021-08-17 23:39:53 +0900

    🎨style: input 스타일 수정 (선화님)

--- src/component/sortingMachine/SortingMachine.js
+++ src/component/sortingMachine/SortingMachine.js
@@ -81,6 +81,10 @@ const Wrapper = styled.div`
       box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
       border: none;
       background-color: gray;
+      outline: none;
+      &:focus {
+          background-color: darkgray;
+      }
     }
   }
   & div {
